516756c620a061029cae7e8a8849ff2c505e112e
Diego Fernandes <diego.fernandes@enjoei.com.br>
2021-04-23 21:51:07 -0300

    new screen User Identification

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  StyleSheet
+} from 'react-native';
+
+export function UserId(){
+  return(
+    true
+  )
+}
+
+// const styles = StyleSheet.create({
+//   container: 
+// })
